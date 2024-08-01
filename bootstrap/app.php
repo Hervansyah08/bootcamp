695ba86ff6d04938b95dac0d4107402a4dfcaa45
hervansyah08 <hervansyahakbar@gmail.com>
2024-08-01 19:39:09 +0700

    multiauth dengan laravel breeze

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Middleware\User;
+use App\Http\Middleware\Admin;
+use App\Http\Middleware\SuperAdmin;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,7 +14,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'user' => User::class,
+            'admin' => Admin::class,
+            'super_admin' => SuperAdmin::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,37 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('auth','user')->group(function () {
+    Route::get('/user', function () {
+        return view('user.index');
+    })->name('user.index');
+});
+Route::middleware('auth','admin')->group(function () {
+    Route::get('/admin', function () {
+        return view('admin.index');
+    })->name('admin.index');
+});
+Route::middleware('auth','super_admin')->group(function () {
+    Route::get('/super-admin', function () {
+        return view('super_admin.index');
+    })->name('super_admin.index');
+});
+
+require __DIR__.'/auth.php';
